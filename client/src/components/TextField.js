4944b3484c35f84660779cede1787e407188b9d9
KreenaShah <102236893+KreenaShah@users.noreply.github.com>
2023-03-17 18:18:25 +0530

    integrated

--- client/src/components/TextField.js
+++ client/src/components/TextField.js
@@ -1,9 +1,14 @@
 //https://stackoverflow.com/questions/61054275/usestate-with-boolean-value-in-react
 import React, { useState , useEffect, useRef } from 'react'
+import axios from 'axios';
 
 function TextField() {
+
     const[isInEditMode , setIsInEditMode] = useState(false);
     const[fieldValue , setFieldValue] = useState("");
+    const[data , setData] = useState({
+      "fullName":""
+    })
     
     const changeEditMode = () => {
         console.log("changeEditMode");
@@ -20,10 +25,27 @@ function TextField() {
 
     const updateDetails = () => {
         console.log("updateDetails function")
-        setIsInEditMode(!isInEditMode);
+        
         console.log(inputFieldValue.current.name,inputFieldValue.current.value);
         setFieldValue(inputFieldValue.current.value);
+        setData({
+          [inputFieldValue.current.name]: inputFieldValue.current.value,
+        });
     }
+    const handleEdit = async () => {
+      console.log("handleEdit function");
+      console.log(inputFieldValue);
+      setIsInEditMode(!isInEditMode);
+      const editedInfo = fieldValue;
+      console.log("Data",data);
+      try {
+        console.log("before editUser api function.........");
+        return await axios.put(`http://localhost:8080/6414546822db73076d5a48c8`,data);
+        // console.log("LOL , Hogayaaa");
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
     function EditableInputField() {
       return (
@@ -59,9 +81,10 @@ function TextField() {
             name="fullName"
             defaultValue={fieldValue}
             ref={inputFieldValue}
+            onChange={updateDetails}
           ></input>
           <button onClick={changeEditMode}>cancel</button>
-          <button onClick={updateDetails}>update</button>
+          <button onClick={() => handleEdit(fieldValue)}>update</button>
           {isInEditMode ? "isInEditMode : TRUE" : "isInEditMode : FALSE"}
         </div>
       ) : (
@@ -70,7 +93,6 @@ function TextField() {
           {isInEditMode ? "isInEditMode : TRUE" : "isInEditMode : FALSE"}
         </div>
       )}
-      
     </>
   );
 }
