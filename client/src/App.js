67acfb3df8634c37827aa7f0bb6331ffdb443eb7
KreenaShah <102236893+KreenaShah@users.noreply.github.com>
2023-03-17 16:20:33 +0530

    commit 1

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import "./App.css";
+import TextField from "./components/TextField";
+
+function App() {
+  // const[data , setData] = useState({
+  //   "email": "",
+  //   "password":""
+  // })
+  // const handleChange = (e) => {
+  //   console.log("HandleChange");
+  //   console.log(e.target.name , e.target.value);
+  // }
+  return (
+    <div className="App">
+      Hey!
+      <TextField/>
+      <TextField />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/TextField.js
@@ -0,0 +1,56 @@
+//https://stackoverflow.com/questions/61054275/usestate-with-boolean-value-in-react
+import React, { useState , useEffect, useRef } from 'react'
+
+function TextField() {
+    const[isInEditMode , setIsInEditMode] = useState(false);
+    const[fieldValue , setFieldValue] = useState("");
+    
+    const changeEditMode = () => {
+        console.log("changeEditMode");
+        console.log(isInEditMode);
+        setIsInEditMode(!isInEditMode);
+    }
+
+    // to check updated state
+    useEffect(() => {
+      console.log(isInEditMode);
+    }, [isInEditMode]);
+    
+    const inputFieldValue = useRef();
+
+    const updateDetails = () => {
+        console.log("updateDetails function")
+        setIsInEditMode(!isInEditMode);
+        console.log(inputFieldValue.current.name,inputFieldValue.current.value);
+        setFieldValue(inputFieldValue.current.value);
+    }
+
+    function EditableInputField() {
+      return (
+        <div>
+          <input
+            type="text"
+            name="email"
+            defaultValue={fieldValue}
+            ref={inputFieldValue}
+          ></input>
+          <button onClick={changeEditMode}>cancel</button>
+          <button onClick={updateDetails}>update</button>
+          {isInEditMode ? "isInEditMode : TRUE" : "isInEditMode : FALSE"}
+        </div>
+      );
+    }
+
+    function DisplayText() {
+      return (
+        <div onDoubleClick={changeEditMode}>
+      {fieldValue}
+      {isInEditMode ? "isInEditMode : TRUE" : "isInEditMode : FALSE"}
+    </div>
+      );
+    }
+    
+  return isInEditMode ? (<EditableInputField />) : (<DisplayText/>);
+}
+
+export default TextField;
